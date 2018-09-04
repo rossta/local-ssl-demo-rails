471da2b7ad05dcb88fad1b0d785ec738a065388b
Ross Kaffenberger <rosskaff@gmail.com>
2018-09-04 14:51:41 -0400

    Fix tests

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import "babel-polyfill"
+import '../hello_vue'
+
 console.log('Hello World from Webpacker')
