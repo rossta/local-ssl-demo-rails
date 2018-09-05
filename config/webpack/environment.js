438134a5b00d1f4e1552ed6218a91dcd8ca43f0a
Ross Kaffenberger <rosskaff@gmail.com>
2018-09-05 13:04:43 -0400

    Implement passing welcome spec

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
+const vue =  require('./loaders/vue')
 
+environment.loaders.append('vue', {
+  test: /\.vue$/,
+  use: [{
+    loader: 'vue-loader',
+    options: {
+      hotReload: false,
+    },
+  }],
+})
+
+environment.loaders.append('vue', vue)
 module.exports = environment
--- spec/system/welcomes_spec.rb
+++ spec/system/welcomes_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.feature "Welcome", :js, type: :system do
   scenario "Visit homepage" do
-    using_app_host('https://subdomain.system-test-demo.test') do
+    using_app_host('https://subdomain.localhost.ross') do
       visit "/"
 
       expect(page).to have_content('Welcome')
       expect(page).to have_content('Hello Vue')
 
-      expect(page).to have_content('Your domain: subdomain.system-demo-test.test')
+      expect(page).to have_content('Your domain: subdomain.localhost.ross')
       expect(page).to have_content('Your protocol: https://')
     end
   end
